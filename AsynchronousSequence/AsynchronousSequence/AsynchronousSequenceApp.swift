e81e1d6fc44f0b7c198747c345d831bb3c8d7333
lukaszluczakk <lukasz.luczak12@gmail.com>
2021-12-10 21:37:20 +0100

    Init project

---
+++ AsynchronousSequence/AsynchronousSequence/AsynchronousSequenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsynchronousSequenceApp.swift
+//  AsynchronousSequence
+//
+//  Created by Łukasz Łuczak on 10/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AsynchronousSequenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
