d607f501d1876f34fa081a016b52e2ee8bf281ad
lukaszluczakk <lukasz.luczak12@gmail.com>
2022-01-02 22:14:53 +0100

    BuildApp test

--- AsynchronousSequence/AsynchronousSequenceTests/AppViewModelTests.swift
+++ AsynchronousSequence/AsynchronousSequenceTests/AppViewModelTests.swift
@@ -6,22 +6,40 @@
 //
 
 import XCTest
+import Combine
 @testable import AsynchronousSequence
 
 class AppViewModelTests: XCTestCase {
     
-    private var service: Service!
+    private var appService: Service!
 
     override func setUpWithError() throws {
-        service = Service()
+        appService = Service()
     }
 
     override func tearDownWithError() throws {
-        service.continuation.finish()
+        appService.continuation.finish()
     }
 
-    func testBuildAppShouldHandleAsyncStream() throws {
-
+    func testBuildAppShouldStartHandleStream() async throws {
+        let vm = AppViewModel(service: appService)
+        
+        let exp = expectation(description: "wait for result")
+        let cancellable = vm.$informations.sink { value in
+            if value.count == 1 {
+                exp.fulfill()
+            }
+        }
+        
+        addTeardownBlock {
+            cancellable.cancel()
+        }
+        
+        
+        
+        appService.continuation.yield(BuildInformation(date: .now, text: "Info 1", type: .info))
+        try await vm.buildApp()
+        wait(for: [exp], timeout: 2)
     }
     
     class Service: AppServiceProtocol {
@@ -38,4 +56,52 @@ class AppViewModelTests: XCTestCase {
             }
         }
     }
+}
+
+class TimeoutTask<Success> {
+  let nanoseconds: UInt64
+  let operation: @Sendable () async throws -> Success
+
+  private var continuation: CheckedContinuation<Success, Error>?
+
+  var value: Success {
+    get async throws {
+      try await withCheckedThrowingContinuation { continuation in
+        self.continuation = continuation
+
+        Task {
+          try await Task.sleep(nanoseconds: nanoseconds)
+          self.continuation?.resume(throwing: TimeoutError())
+          self.continuation = nil
+        }
+
+        Task {
+          let result = try await operation()
+          self.continuation?.resume(returning: result)
+          self.continuation = nil
+        }
+      }
+    }
+  }
+
+  func cancel() {
+    continuation?.resume(throwing: CancellationError())
+    continuation = nil
+  }
+
+  init(
+    seconds: TimeInterval,
+    operation: @escaping @Sendable () async throws -> Success
+  ) {
+    self.nanoseconds = UInt64(seconds * 1_000_000_000)
+    self.operation = operation
+  }
+}
+
+extension TimeoutTask {
+  struct TimeoutError: LocalizedError {
+    var errorDescription: String? {
+      return "The operation timed out."
+    }
+  }
 }
